f4741ee47081bd30a13f96b614d77ebcbd64f15e
Jasery <730333708@qq.com>
2017-07-29 08:47:53 +0800

    新增添加页面

---
+++ src/components/DetailItem.vue
@@ -0,0 +1,75 @@
+<template>  
+    <div class="detail-item">
+        <div class="detail-content">
+            <div class="isComplete">
+                <input v-model="isComplete" type="checkbox">
+            </div>
+            <div class="detail-text">
+                <input v-model="detailText" type="text" placeholder="点击编辑">
+            </div>
+            <div class="item-remove">删除</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailItem',
+  data: function() {
+      return {
+          isComplete: false,
+          detailText: ''
+      }
+  }
+
+}
+</script>
+
+<style>
+    .detail-item {
+        overflow-x: auto;
+    }
+    .detail-item::-webkit-scrollbar {
+        display: none;
+    }
+    .detail-item .detail-content {
+        width: 120%;                
+        overflow: hidden;
+        border-bottom: 1px solid #e5e5e5;
+        height: 40px;
+        line-height: 40px;
+    }
+    .detail-item .detail-content > div {
+        float: left;
+    }
+    .detail-item .detail-content .isComplete {
+        width: 10%;
+        padding: 5px 0;   
+    }
+    .detail-item .detail-content .isComplete input[type=checkbox] {
+        height: 25px;
+        width: 25px;
+        margin: 2px;
+    }
+    .detail-item .detail-content .detail-text {
+        width: 80%;
+    }
+    .detail-item .detail-content .detail-text input {
+        height: 25px;
+        border: none;
+        line-height: 25px;
+        outline: none;
+        padding: 5px 0;   
+        font-size: 18px;
+    }
+    .detail-item .detail-content .item-remove {
+        width: 10%;
+        background-color: red; 
+        height: 40px;
+        line-height: 40px;
+        text-align: center;
+        color: white;
+        font-size: 18px;
+    }
+
+</style>
--- src/views/TodoAdd.vue
+++ src/views/TodoAdd.vue
@@ -1,11 +1,107 @@
 <template>
-    <div>这是Todo Add</div>
+    <div class="todo-add">
+        <div class="todo-title">
+            <input v-model="todoTitle" placeholder="未命名清单"> 
+            <div class="clear-input" v-show="todoTitle" @click="clearInput">×</div>
+        </div>
+        <div class="todo-detail">
+            <!-- <div class="detail-item">
+                <div class="detail-content">
+                    <div class="isComplete">
+                        <input type="checkbox">
+                    </div>
+                    <div class="detail-text">
+                        <input type="text" placeholder="点击编辑">
+                    </div>
+                    <div class="item-remove">删除</div>
+                </div>
+            </div> -->
+            <detail-item v-for="detail in details" :key="detail"></detail-item>
+            <div class="add-detail-item">
+                添加事项
+            </div>
+        </div>
+        <button class="detail-save">保存</button>
+    </div>
 </template>
 <script>
+import DetailItem from '../components/DetailItem.vue'
 export default {
-  
+  name: 'TodoAdd',
+  components: {
+    DetailItem
+  },
+  data: function() {
+      return {
+          todoTitle: '',
+          details: [{}]
+      }
+  },
+  methods: {
+      clearInput() {
+          this.todoTitle = ''
+      }
+  }
 }
 </script>
 <style>
-
+    .todo-add {
+        margin: 10px 0;
+        /* position: relative; */
+    }
+    .todo-title {
+        position: relative;
+    }
+    .todo-title input {
+        width: 100%;
+        height: 50px;
+        border-left: none;
+        border-right: none;
+        border-top: 1px solid #e5e5e5;
+        border-bottom: 1px solid #e5e5e5;
+        text-indent: 20px;
+        line-height: 50px;
+        font-size: 20px;
+        outline: none;
+    }
+    .todo-title .clear-input {
+        position: absolute;
+        color: white;
+        background-color: #0EBF5A;
+        height: 20px;
+        width: 20px;
+        line-height: 16px; 
+        text-align: center;
+        font-size: 20px;
+        border-radius: 50%;
+        top: 20px;
+        right: 20px;
+    }
+    .todo-detail {
+        margin-left: 10px;
+        background-color: white;
+    }
+    .add-detail-item {
+        color: #0EBF5A;
+        font-size: 18px;
+        line-height: 40px;
+        text-indent: 10px;
+    }
+    .add-detail-item:before {
+        content: "+";
+        font-size: 25px;
+    }
+    .detail-save {
+        height: 50px;
+        background-color: #0EBF5A;
+        border: none;
+        border-radius: 5px;
+        color: white;
+        width: 90%;
+        position: absolute;
+        bottom: 20px;
+        left: 50%;
+        transform: translateX(-50%);
+        font-size: 18px;
+    }
 </style>
